6606945c53fd72fafcdd1a6b4bc4f84760087ab4
abisummers <hello@abisummers.com>
2018-11-18 11:47:49 +0100

    added fileupload config

---
+++ config/file-uploader.js
@@ -0,0 +1,22 @@
+const multer = require("multer");
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.cloudinary_NAME,
+  api_key: process.env.cloudinary_KEY,
+  api_secret: process.env.cloudinary_SECRET
+});
+
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "users-pictures",
+  // allowedFormats: ["jpg", "png"],
+  params: {
+    resource_type: "raw"
+  }
+});
+
+const fileUploader = multer({ storage });
+
+module.exports = fileUploader;
